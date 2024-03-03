5f1586d7913a0a90736817ca2479a0426f597105
Shivam Uniyal <shivamuniyal1810@gmail.com>
2024-03-03 23:25:35 +0530

    Add: functions to calc basic arithmetic operations

---
+++ script.js
@@ -0,0 +1,16 @@
+// functions for basic arithmetic operations
+function add(a,b){
+    return a+b;
+}
+
+function sub(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
