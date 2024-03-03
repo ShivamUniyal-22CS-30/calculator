78d288f90999e48ab8ad0cbdc07f2f143a6dd657
Shivam Uniyal <shivamuniyal1810@gmail.com>
2024-03-03 23:51:27 +0530

    Add: operate(operator,firstNum,secondNum)

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ function multiply(a,b){
 function divide(a,b){
     return a/b;
 }
+
+let firstNum = '';
+let operator = '';
+let secondNum = '';
+
+function operate(operator,firstNum,secondNum){
+    if(operator == "+"){
+        return add(firstNum,secondNum);
+    }
+    else if(operator == "-"){
+        return sub(firstNum,secondNum);
+    }
+    else if(operator == "x"){
+        return multiply(firstNum,secondNum);
+    }
+    else if(operator == "/"){
+        return divide(firstNum,secondNum);
+    }
+};
